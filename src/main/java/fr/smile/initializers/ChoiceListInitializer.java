e70a05d6f260a2b7bd5e4bdf8ece9fb0d11cc84a
alainncls <alain.nicolas.perso@gmail.com>
2015-03-05 10:11:18 +0100

    [Ex 3.1] Exercise 3.1

---
+++ src/main/java/fr/smile/initializers/ChoiceListInitializer.java
@@ -0,0 +1,87 @@
+package fr.smile.initializers;
+
+import org.jahia.services.content.JCRPropertyWrapper;
+import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
+import org.jahia.services.content.nodetypes.ValueImpl;
+import org.jahia.services.content.nodetypes.initializers.ChoiceListValue;
+import org.jahia.services.content.nodetypes.initializers.ModuleChoiceListInitializer;
+import org.jahia.services.content.nodetypes.renderer.AbstractChoiceListRenderer;
+import org.jahia.services.content.nodetypes.renderer.ModuleChoiceListRenderer;
+import org.jahia.services.render.RenderContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import java.util.*;
+
+public class ChoiceListInitializer extends AbstractChoiceListRenderer implements ModuleChoiceListInitializer, ModuleChoiceListRenderer {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChoiceListInitializer.class);
+
+    private String key;
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<ChoiceListValue> getChoiceListValues(ExtendedPropertyDefinition epd, String param, List<ChoiceListValue> values,
+                                                     Locale locale, Map<String, Object> context) {
+
+        List<ChoiceListValue> myChoiceList = new ArrayList<ChoiceListValue>();
+
+        if (context == null) {
+            return myChoiceList;
+        }
+
+        HashMap<String, Object> myPropertiesMap = null;
+
+        myChoiceList.add(new ChoiceListValue("Référence #1","1"));
+        myChoiceList.add(new ChoiceListValue("Référence #2","2"));
+        myChoiceList.add(new ChoiceListValue("Référence #3","3"));
+
+        //Return the list
+        return myChoiceList;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getStringRendering(RenderContext context, JCRPropertyWrapper propertyWrapper) throws RepositoryException {
+        final StringBuilder sb = new StringBuilder();
+
+        if (propertyWrapper.isMultiple()) {
+            sb.append('{');
+            final Value[] values = propertyWrapper.getValues();
+            for (Value value : values) {
+                sb.append('[').append(value.getString()).append(']');
+            }
+            sb.append('}');
+        } else {
+            sb.append('[').append(propertyWrapper.getValue().getString()).append(']');
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getStringRendering(Locale locale, ExtendedPropertyDefinition propDef, Object propertyValue) throws RepositoryException {
+        return "[" + propertyValue.toString() + "]";
+    }
+}
